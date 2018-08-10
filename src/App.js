e575aed6b934d5d3a7a6b79b00e333ad37765d2a
Wendy Gomez <suleyma_rz@hotmail.com>
2018-08-10 14:44:10 -0500

    Fetch to Spotify API to get playlists

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './assets/Spotify_Logo_RGB_White.png';
 //import './App.css';
 import './styles/app.sass';
-
 import Notification from './js/components/Notification';
 
 import Aggregate from './js/components/Aggregate';
@@ -11,119 +10,146 @@ import InputComponent from './js/components/InputComponent';
 import Filter from './js/components/Filter';
 import PlaylistItem from './js/components/PlaylistItem';
 
+import querystring from 'query-string'
 
+let data = {
+    user: {
+        name: "Suleyma",
+        playlists: [
+            {
+                idplaylist: "123",
+                name: "Rock",
+                songs: [
+                    {
+                        idsong: "1",
+                        title: "Beat it",
+                        artist: "Michael Jackson",
+                        duration: 305
+                    },
+                    {
+                        idsong: "2",
+                        title: "Bohemian Rhaphsody",
+                        artist: "Queen",
+                        duration: 400
+                    },
+                    {
+                        idsong: "3",
+                        title: "Always",
+                        artist: "Bon Jovi",
+                        duration: 305
+                    }],
+            },
+            {
+                idplaylist: "124",
+                name: "Sad songs",
+                songs: [
+                    {
+                        idsong: "4",
+                        title: "Fix you",
+                        artist: "Coldplay",
+                        duration: 450
+                    },
+                    {
+                        idsong: "5",
+                        title: "I'm with you",
+                        artist: "Avril Lavigne",
+                        duration: 295
+                    },
+                    {
+                        title: "Someone like you",
+                        artist: "Adele",
+                        duration: 279
+                    }],
+            },
+            {
+                idplaylist: "125",
+                name: "Same songs",
+                songs: [
+                    {
+                        idsong: "6",
+                        title: "Beat it",
+                        artist: "Michael Jackson",
+                        duration: 305
+                    },
+                    {
+                        idsong: "7",
+                        title: "Beat it",
+                        artist: "Michael Jackson",
+                        duration: 305
+                    },
+                    {
+                        idsong: "8",
+                        title: "Beat it",
+                        artist: "Michael Jackson",
+                        duration: 305
+                    }],
+            },
+            {
+                idplaylist: "126",
+                name: "Party songs",
+                songs: [
+                    {
+                        idsong: "9",
+                        title: "Beat it",
+                        artist: "Michael Jackson",
+                        duration: 305
+                    },
+                    {
+                        idsong: "8",
+                        title: "Beat it",
+                        artist: "Michael Jackson",
+                        duration: 305
+                    },
+                    {
+                        idsong: "9",
+                        title: "Beat it",
+                        artist: "Michael Jackson",
+                        duration: 305
+                    }],
+            }
+        ]
+    }
+};
 
 class App extends Component {
   constructor(props){
     super(props);
-    let data ={
-      user:{
-        name:"Suleyma",
-        playlists:[
-          {
-              idplaylist:"123",
-              name: "Rock",
-            songs: [
-              {
-                  idsong:"1",
-                title:  "Beat it",
-                artist: "Michael Jackson",
-                duration: 305
-              },
-              {
-                  idsong:"2",
-                title:  "Bohemian Rhaphsody",
-                artist: "Queen",
-                duration: 400
-              },
-              {
-                  idsong:"3",
-                title:  "Always",
-                artist: "Bon Jovi",
-                duration: 305
-              }],
-          },
-          { idplaylist:"124",
-              name: "Sad songs",
-            songs: [
-              {
-                  idsong:"4",
-                title:  "Fix you",
-                artist: "Coldplay",
-                duration: 450
-              },
-              {
-                  idsong:"5",
-                title:  "I'm with you",
-                artist: "Avril Lavigne",
-                duration: 295
-              },
-              {
-                title:  "Someone like you",
-                artist: "Adele",
-                duration: 279
-              }],
-          },
-          { idplaylist:"125",
-              name: "Same songs",
-            songs: [
-              {
-                  idsong:"6",
-                title:  "Beat it",
-                artist: "Michael Jackson",
-                duration: 305
-              },
-              {
-                  idsong:"7",
-                title:  "Beat it",
-                artist: "Michael Jackson",
-                duration: 305
-              },
-              {
-                  idsong:"8",
-                title:  "Beat it",
-                artist: "Michael Jackson",
-                duration: 305
-              }],
-          },
-          { idplaylist:"126",
-              name: "Party songs",
-            songs: [
-              {
-                  idsong:"9",
-                title:  "Beat it",
-                artist: "Michael Jackson",
-                duration: 305
-              },
-              {
-                  idsong:"8",
-                title:  "Beat it",
-                artist: "Michael Jackson",
-                duration: 305
-              },
-              {
-                  idsong:"9",
-                title:  "Beat it",
-                artist: "Michael Jackson",
-                duration: 305
-              }],
-          }
-        ]
-      }
-    };
-
     this.state={
-      filterString:"songs",
+      filterString:"",
       //serverDataSample: data
     }
   }
-  render() {
-    let playlistsToRender = this.state.serverDataSample ? this.state.serverDataSample.user.playlists.filter(playlist=>
+
+  componentDidMount(){
+      let parsed = querystring.parse(window.location.search);
+      let accessToken=parsed.access_token;
+
+      //Get the user info
+      fetch('https://api.spotify.com/v1/me',{
+          headers:{'Authorization': 'Bearer ' + accessToken}
+      }).then((response) => response.json()).then((data) => {
+          this.setState({serverDataSample:{user:{name:( data.display_name && data.display_name!==null )? data.display_name : data.id}}});
+          return true;
+      });
+
+      //Get the user playlists
+      fetch('https://api.spotify.com/v1/me/playlists',{
+          headers:{'Authorization': 'Bearer ' + accessToken}
+      }).then((response) => response.json()).then((data) => {
+          let currentData =this.state.serverDataSample;
+          currentData.playlists=data.items;
+          this.setState({serverDataSample:currentData});
+          console.log(data);
+      });
+  }
+
+  render(){
+    let playlistsToRender = this.state.serverDataSample && this.state.serverDataSample.playlists ? this.state.serverDataSample.playlists.filter(playlist=>
       playlist.name.toLowerCase().includes(this.state.filterString.toLowerCase())
     ) : [];
     return (
       <div className="App">
-      { this.state.serverDataSample ?
+          { this.state.serverDataSample && this.state.serverDataSample !==null ?
         <div>
           <nav className="menu-top">
               <div className="col-20">
@@ -152,26 +178,33 @@ class App extends Component {
                     </p>
                     <InputComponent/>
                 </div>
-                <div className="cols col-50 playlistComponent"></div>
                 <div className="cols col-50 playlistComponent">
-                    <p className="first-title">
-                        {this.state.serverDataSample.user.name}'s Playlists
-                    </p>
-                    <Filter onTextChange={text=>this.setState({filterString:text})}/>
-                    <div className="playlistsContent">
-                        {
-                            playlistsToRender.map(playlist=>
-                                <PlaylistItem key={playlist.name} info={playlist}/>
-                            )
+                    {this.state.serverDataSample.user ?
+                    <div>
+                        <p className="first-title">
+                            {this.state.serverDataSample.user.name}'s Playlists
+                        </p>
+                        {this.state.serverDataSample.playlists ?
+                            <div>
+                                <Filter onTextChange={text=>this.setState({filterString:text})}/>
+                                <div className="playlistsContent">
+                                    {
+                                        playlistsToRender.map(playlist=>
+                                            <PlaylistItem key={playlist.name} info={playlist}/>
+                                        )
+                                    }
+                                </div>
+                                <button className="btn btn-full-width">Add songs</button>
+                            </div> : <div><p className="ornament-center">No playlists found</p></div>
                         }
-                    </div>
-                    <button className="btn btn-full-width">Add songs</button>
+                    </div> : <div></div>
+                    }
                 </div>
             </div>
         </div> :
         <Notification title="Sign in" className="notification-content-sm">
           <p>Oops! It look like you're not signed in :C </p>
-          <button className="btn"> Sign in with Spotify</button>
+          <button className="btn" onClick={() => window.location = 'http://localhost:8888/login'}> Sign in with Spotify</button>
         </Notification>                
       }
       </div>
--- src/js/components/PlaylistItem.js
+++ src/js/components/PlaylistItem.js
@@ -4,19 +4,19 @@ class PlaylistItem extends Component{
     render(){
         return(
             <div className="playlist-Item">
-                <div className="cols col-10">
-                    <img className="playlist-Image circle-preview" src="https://hmrock.com.br/wp-content/uploads/2016/09/004972672_500.jpg"/>
+                <div className="cols col-15">
+                    <img className="playlist-Image circle-preview" src={this.props.info.images[0].url}/>
                 </div>
                 <div className="cols col-75 playlist-Info">
                     <h3>{this.props.info.name}</h3>
                     <ul className="playlist-Songs">
-                        {this.props.info.songs.map(song=>
+                        {/*this.props.info.songs.map(song=>
                             <li key={song.idsong}>{song.title} - {song.artist}</li>
                         )
-                        }                    
+                        */}
                     </ul>
                 </div>
-                <div className="cols col-15 form-checkbox">
+                <div className="cols col-10 form-checkbox">
                     <input type="checkbox" id={"checkbox-"+this.props.info.idplaylist}/>
                     <label for={"checkbox-"+this.props.info.idplaylist}></label>
                 </div>
